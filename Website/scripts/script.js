43b5895c3cf3d5d290e79140a8a5553c2ad53c3e
nboj <christian.auman@outlook.com>
2021-05-11 17:36:50 -0400

    Website Update

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,3 +1,13 @@
-function display() {
-    
-}
+function copyToClipboard(textID, buttonID) {
+    var r = document.createRange();
+    r.selectNode(document.getElementById(textID));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();  
+    var buttons = $("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].innerHTML = "Copyy";
+    }
+    $("#button1").innerHTML = "Copied"; 
+} 
