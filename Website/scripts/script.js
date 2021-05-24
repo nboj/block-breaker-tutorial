1cc433b2a31ba76e96633c9c851302b87055fd1e
nboj <christian.auman@outlook.com>
2021-05-24 18:26:51 -0400

    Update script.js

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,7 +1,3 @@
-// $(window).on('beforeunload', function() {
-//     $(window).scrollTop(0);
-// });
-
 function copyToClipboard(textID, buttonID) {
     var r = document.createRange();
     r.selectNode(document.getElementById(textID));
