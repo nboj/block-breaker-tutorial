c79b72e33f875baf091e472fb475cf0c042f53bc
Christian <christian.auman@outlook.com>
2021-05-25 09:57:47 -0400

    Update

--- Block Breaker In Class/Block breaker 2.0/Assets/Scripts/Control/Ball.cs
+++ Block Breaker In Class/Block breaker 2.0/Assets/Scripts/Control/Ball.cs
@@ -27,7 +27,7 @@ public class Ball : MonoBehaviour
             else 
             {
                 Vector3 position = paddle.transform.position;
-                transform.position = new Vector3(position.x, transform.position.y, 0);
+                transform.position = new Vector3(Mathf.Clamp(position.x, -6.5f, 6.5f), transform.position.y, 0);
             }
         }
     }
