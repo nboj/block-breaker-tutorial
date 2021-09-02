cb42c686c803cfc493e69710dea361e7a0eae1c3
Christian <christian.auman@outlook.com>
2021-09-02 12:44:41 -0400

    Uppp

---
+++ Website/Block-Breaker-Website/scripts/script.js
@@ -0,0 +1,13 @@
+function copyToClipboard(textID, buttonID) {
+    var r = document.createRange();
+    r.selectNode(document.getElementById(textID));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();  
+    var buttons = $("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].innerHTML = "Copy";
+    }
+    $("#" + buttonID)[0].innerHTML = "Copied"; 
+}  
